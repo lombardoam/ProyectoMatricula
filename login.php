f40c2e5a4e207de33c547c22943614949bda4a62
Rychiv4 <rick_valladares@hotmail.com>
2015-07-31 20:10:43 -0600

    Diseñando el login.php

--- login.php
+++ login.php
@@ -1,8 +1,27 @@
 <?php
-   $usuario = $_POST['usuario'];
-   $contrasena = $_POST['contra'];
 
-    $conexion = mysqli_connect('localhost','root','','matricula');
-    $query = "SELECT * FROM usuarios WHERE usuario=".$usuario. "and contrasena =".$contrasena;
+   if(isset($_POST['usuario']) && isset($_POST['contra'])){
+      $usuario = $_POST['usuario'];
+      $contrasena = $_POST['contra'];
+      $query = "SELECT * FROM usuarios WHERE nombre_usuario = '" . $usuario . "' AND contrasena = '" . $contrasena . "';";
+
+      try {
+         $conexion = mysqli_connect('localhost','root','','matricula');
+         $exec = mysqli_query($conexion, $query);
+         if($result = mysqli_fetch_row($exec)){
+            echo "bien";
+            session_start();
+            $_SESSION['nombre'] = $result[1];
+            $_SESSION['apellido'] = $result[2];
+            $_SESSION['usuario'] = $result[3];
+         }else{
+            echo "mal";
+         }
+      } catch(mysqli_exception $e){
+         echo $e;
+      }
+   }else{
+      header('Location:index.html');
+   }
 
 ?>
