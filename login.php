0e787157ab616b283b3891133157d1c38e269f7d
Rychiv4 <rick_valladares@hotmail.com>
2015-07-31 20:43:22 -0600

    Completando el login.php

--- login.php
+++ login.php
@@ -14,6 +14,23 @@
             $_SESSION['nombre'] = $result[1];
             $_SESSION['apellido'] = $result[2];
             $_SESSION['usuario'] = $result[3];
+            switch ($result[5]){
+               case 1: //Administrador
+                  header('Location:adminSistema/index.php');
+                  break;
+               case 2; //Docente
+                  header('Location:maestros/index.html');
+                  break;
+               case 3; //Alumno
+                  header('Location:index.html?alumno');
+                  break;
+               case 4; //Encargado
+                  header('Location:adminmatricula/index.php');
+                  break;
+               case 5; //Contador
+                  header('Location:index.html?contador');
+                  break;
+            }
          }else{
             echo "mal";
          }
