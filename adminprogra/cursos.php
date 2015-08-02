9c93fe491623026900b2ea9c78e1c2d33174df4b
Rychiv4 <rick_valladares@hotmail.com>
2015-08-01 22:49:50 -0600

    Agregando sesiones a la interfaz de adminprogra

--- adminprogra/cursos.php
+++ adminprogra/cursos.php
@@ -1,4 +1,17 @@
-<html>
+<?php
+   session_start();
+   if(!(
+      isset($_SESSION['nombre']) &&
+      isset($_SESSION['apellido']) &&
+      isset($_SESSION['usuario']) &&
+      isset($_SESSION['tipo_usuario']) &&
+      isset($_SESSION['num_cuenta'])
+      )
+   ){
+      header('Location:../index.php?no_aut');
+   }
+?>
+ <html>
   <head>
 
     <link href="themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
