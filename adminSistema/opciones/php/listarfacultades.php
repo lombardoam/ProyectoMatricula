2f3a7f486bffb82c5b34b4b46d097607bafc0c00
Rychiv4 <rick_valladares@hotmail.com>
2015-08-01 22:43:15 -0600

    Agregando sesiones a la interfa de adminSistema

--- adminSistema/opciones/php/listarfacultades.php
+++ adminSistema/opciones/php/listarfacultades.php
@@ -1,4 +1,15 @@
 <?php
+   session_start();
+   if(!(
+      isset($_SESSION['nombre']) &&
+      isset($_SESSION['apellido']) &&
+      isset($_SESSION['usuario']) &&
+      isset($_SESSION['tipo_usuario']) &&
+      isset($_SESSION['num_cuenta'])
+      )
+   ){
+      header('Location:../index.php?no_aut');
+   }
   header('Content-type: application/json');
   require '../require/conexion.php';
   //Get records from database
