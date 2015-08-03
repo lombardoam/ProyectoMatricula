e06699a5c78d4e825cd7723b6ea9ddff4c061f06
Rychiv4 <rick_valladares@hotmail.com>
2015-08-02 22:55:47 -0600

    Programando interfaz de maestros

--- maestros/js/scripts.js
+++ maestros/js/scripts.js
@@ -1,3 +1,13 @@
 $("document").ready(function(){
 
 });
+function getEval(){
+   $.ajax({
+      method: "POST",
+      url: "php/getdata.php",
+      data: "eval",
+      sucess: function(data){
+         alert(data);
+      }
+   });
+}
