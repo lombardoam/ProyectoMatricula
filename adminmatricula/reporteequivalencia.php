d6b3e1cb9bf78bdcb12e5f81475e1836133056aa
juanreyesmonroy <!juanreyes2015!>
2015-08-11 00:53:25 -0600

    Informe de equivalencias de la interfaz de Administración de Matrícula

--- adminmatricula/reporteequivalencia.php
+++ adminmatricula/reporteequivalencia.php
@@ -15,54 +15,57 @@
    }
 ?>
 
-<meta charset="UTF-8">
-
-<!--Código HTML editable-->
-
-       <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div align="center">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Equivalencias
-
-                        </h1>
-                        <ol class="breadcrumb">
-
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
-
-                </div></div>
-            <!-- /.container-fluid -->
-            <!-- /.Colocar aquí el código y contenido que se desee poner en la página -->
-            <div align="center">
-
-<?php
-
-require'equivalencia.php';
-
-?>
-
-            <br><br>
-
-  <form>
-<input type='button' onclick='window.print();' value='Impresión' class="btn btn-primary btn-lg" role="button">
-                </form>           </div>
-
-            <br><br>
-
-        </div>
-        <!-- /#page-wrapper -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Reportes - Equivalencias</title>
+   <link rel="stylesheet" href="css/bootstrap.css">
+   <script src="js/bootstrap.js"></script>
+</head>
+<body><br><br>
+    <div align="center">
+        <img src="images/UJCV.jpg" alt="UJCV Logo" width="10%" height="10%" /><h1>Equivalencias</h1>
+  <div class="container">
+      <center><a href="javascript:window.print(); void 0;"  class="btn btn-default"><i class="glyphicon glyphicon-print"></i></a><br></center></div><br>
+        <div class="table-responsive">
+   <table class="table table-bordered">
+      <theader>
+         <tr>
+            <th>Codigo</th>
+            <th>Nombre Universidad</th>
+            <th>Asignatura</th>
+            <th>Clase equivalencia</th>
+            <th>Cuenta</th>
+            <th>Tipo equivalencia</th>
+            <th>Comentarios</th>
 
 
-    <!-- /#wrapper -->
+         </tr>
+      </theader>
+      <tbody >
+        <?php
+            $conexion = mysqli_connect('localhost','root','','matricula');
+            $sql = "SELECT equivalencias.id_interna, equivalencias.codigo_eq, equivalencias.nombre_universidad, cursos.nombre_curso, equivalencias.codigo_clase_equivalencia, equivalencias.num_cuenta, tipo_equivalencias.descripcion, equivalencias.comentarios FROM equivalencias INNER JOIN cursos INNER JOIN tipo_equivalencias WHERE equivalencias.id_curso = cursos.id_curso AND equivalencias.id_tipo = tipo_equivalencias.id_tipo ORDER BY id_interna;;";
+            $query = mysqli_query($conexion, $sql);
+            while($rows = mysqli_fetch_assoc($query)){
+               echo "
+                  <tr>
+                     <td>$rows[codigo_eq]</td>
+                     <td>$rows[nombre_universidad]</td>
+                     <td>$rows[nombre_curso]</td>
+                     <td>$rows[codigo_clase_equivalencia]</td>
+                     <td>$rows[num_cuenta]</td>
+                     <td>$rows[descripcion]</td>
+                     <td>$rows[comentarios]</td>
 
+                  </tr>
+               ";
+            }
+         ?>
 
-  </body>
+      </tbody>
+            </table></div>
+   </div>
+</body>
 </html>
