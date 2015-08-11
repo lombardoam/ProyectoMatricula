91e4ade2d069fba4acfd4c01ab66256ec4eef3c5
juanreyesmonroy <!juanreyes2015!>
2015-08-10 23:49:53 -0600

    Reportes de docentes en interfaces de Programación, Administración de Matrícula y Primer Ingreso

--- primeringreso/reportedocentes.php
+++ primeringreso/reportedocentes.php
@@ -15,53 +15,58 @@
    }
 ?>
 
-<meta charset="UTF-8">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Reportes - Docentes</title>
+   <link rel="stylesheet" href="css/bootstrap.css">
+   <script src="js/bootstrap.js"></script>
+</head>
+<body><br><br>
+    <div align="center">
+        <img src="images/UJCV.jpg" alt="UJCV Logo" width="10%" height="10%" /><h1>Docentes</h1>
+  <div class="container">
+      <center><a href="javascript:window.print(); void 0;"  class="btn btn-default"><i class="glyphicon glyphicon-print"></i></a><br></center></div><br>
+        <div class="table-responsive">
+   <table class="table table-bordered">
+      <theader>
+         <tr>
+            <th>Codigo</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>Género</th>
+            <th>Teléfono</th>
+            <th>E-mail</th>
+            <th>Código Carrera</th>
+            <th>Puesto</th>
 
-<!--Código HTML editable-->
+         </tr>
+      </theader>
+      <tbody >
+        <?php
+            $conexion = mysqli_connect('localhost','root','','matricula');
+            $sql = "SELECT empleados.id_empleado, empleados.codigo_empleado, empleados.nombres, empleados.apellidos, empleados.genero, empleados.telefono, empleados.email, carreras.nombre_carrera, puestos.descripcion FROM empleados INNER JOIN carreras INNER JOIN puestos WHERE empleados.codigo_carrera = carreras.codigo_carrera AND empleados.id_puesto = puestos.id_puesto AND (puestos.descripcion='Coordinador de carrera' OR puestos.descripcion='docente') ORDER BY id_empleado;;";
+            $query = mysqli_query($conexion, $sql);
+            while($rows = mysqli_fetch_assoc($query)){
+               echo "
+                  <tr>
+                     <td>$rows[codigo_empleado]</td>
+                     <td>$rows[nombres]</td>
+                     <td>$rows[apellidos]</td>
+                     <td>$rows[genero]</td>
+                     <td>$rows[telefono]</td>
+                     <td>$rows[email]</td>
+                     <td>$rows[nombre_carrera]</td>
+                     <td>$rows[descripcion]</td>
 
-       <div id="page-wrapper">
+                  </tr>
+               ";
+            }
+         ?>
 
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div align="center">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Docentes de la UJCV
-                        </h1>
-                        <ol class="breadcrumb">
-
-                        </ol>
-                    </div>
-                    </div></div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-            <!-- /.Colocar aquí el código y contenido que se desee poner en la página -->
-            <div align="center">
-
-<?php
-
-require'catedraticos.php';
-
-?>
-
-            <br><br>
-
-  <form>
-<input type='button' onclick='window.print();' value='Impresión' class="btn btn-primary btn-lg" role="button">
-                </form>           </div>
-
-            <br><br>
-
-        </div>
-        <!-- /#page-wrapper -->
-
-
-    <!-- /#wrapper -->
-
-
-  </body>
+      </tbody>
+            </table></div>
+   </div>
+</body>
 </html>
