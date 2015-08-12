e1abb6fa29d57b0479b38e8df321ed11d5df2fd4
juanreyesmonroy <!juanreyes2015!>
2015-08-12 05:46:30 -0600

    Buscador para asignaturas en interfaces de Primer Ingreso y  Administración de Matrícula (por Plan de Estudio)

--- primeringreso/asignaturas.php
+++ primeringreso/asignaturas.php
@@ -18,11 +18,22 @@
   require 'conexion.php';
 
 //Get record count
+if (empty($_POST['nombre'])){
+            }
 		$result = mysqli_query($conexion, "SELECT COUNT(*) AS RecordCount FROM cursos WHERE periodo ='1'");
 		$row = mysqli_fetch_array($result);
 		$recordCount = $row['RecordCount'];
   //Get records from database
+if (empty($_POST['nombre'])){
   $result = mysqli_query($conexion, "SELECT * FROM cursos WHERE periodo ='1'");
+    }
+
+    else
+    {
+
+        $nombre = $_POST['nombre'];
+        $result = mysqli_query($conexion, "SELECT * FROM cursos WHERE periodo ='1' AND id_plan_estudio LIKE '%$nombre%'");
+}
 
   //Add all records to an array
   $rows = array();
