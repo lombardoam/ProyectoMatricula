8d98ba67beecc925f8c2d6b5852e62aab1b85319
lombardoam <ardonmejial@gmail.com>
2015-08-13 21:53:53 -0600

    Pre matricula

--- estudiantes/prematricula.php
+++ estudiantes/prematricula.php
@@ -33,9 +33,10 @@ require'header.php';
             <!-- /.container-fluid -->
             <!-- /.Colocar aquí el código y contenido que se desee poner en la página -->
             <div align="center">
+
             <form name="form" action="prematricula.php" method="post">
 
-    <table><thead><tr>
+            <table><thead><tr>
     <th></th>
     <th>ID</th>
     <th>Asignatura</th>
@@ -43,33 +44,41 @@ require'header.php';
     <th>Hora Final</th>
     <th>Dias</th>
     <th>Sección</th>
+    <th>Período</th>
     <th>Esatatus</th></tr></thead>
     <tbody>
+
+    <h4>Prematricula - (Plan de <?php $conexion = mysqli_connect("localhost", "root", "", "matricula");
+    $sql=mysqli_query($conexion, "SELECT nombre_plan FROM planes_estudio WHERE id_carrera = $_SESSION[id_carrera]"); while($row= mysqli_fetch_assoc($sql)){echo $row['nombre_plan'];}?>)</h4><h5><font color="red">Le recomendamos que pre matricule las clases de períodos atrasados que no ha cursado</font></h5><br><br>
+
     <?php
     $conexion = mysqli_connect("localhost", "root", "", "matricula");
-    $sql=mysqli_query($conexion, "SELECT id_programacion, nombre_curso, hora_inicio, hora_termina, dias, seccion, estatus_curso FROM cursos INNER JOIN programacion_cursos INNER JOIN planes_estudio INNER JOIN carreras WHERE cursos.id_curso = programacion_cursos.id_curso AND cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND planes_estudio.id_carrera = carreras.id_carrera AND carreras.id_carrera = $_SESSION[id_carrera] ORDER BY cursos.periodo ASC");
+    $sql=mysqli_query($conexion, "SELECT id_programacion, nombre_curso, hora_inicio, hora_termina, dias, seccion, periodo, estatus_curso FROM cursos INNER JOIN programacion_cursos INNER JOIN planes_estudio INNER JOIN carreras WHERE cursos.id_curso = programacion_cursos.id_curso AND cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND planes_estudio.id_carrera = carreras.id_carrera AND carreras.id_carrera = $_SESSION[id_carrera] ORDER BY cursos.periodo ASC ");
 
     $i = 0;
 
     while($row=mysqli_fetch_array($sql)){
     ?>
 
+
     <tr>
     <td width="5%"><input class="check" type="checkbox" name="select[]" value="<?php echo $i++; ?>"/></td>
-    <td><input class="seccion" type="text" name="id[]" readonly id="id" value="<?php echo $row['id_programacion']; ?>"></td>
-    <td><input class="asignaturas" type="text" name="curso[]" readonly id="nombre_curso" value="<?php echo $row['nombre_curso']; ?>"></td>
+    <td><input type="text" name="id[]" readonly id="id" value="<?php echo $row['id_programacion']; ?>"></td>
+    <td><input type="text" name="curso[]" readonly id="nombre_curso" value="<?php echo $row['nombre_curso']; ?>"></td>
     <td width="30px"><input class="horas" type="text" name="inicio[]" readonly id="hora_inicio" value="<?php echo $row['hora_inicio']; ?>"></td>
-    <td><input class="horas" type="text" name="termina[]" readonly id="hora_termina" value="<?php echo $row['hora_termina']; ?>"></td>
-    <td><input class="dias" type="text" name="dias[]" readonly id="dias" value="<?php echo $row['dias']; ?>"></td>
-    <td><input class="seccion" type="text" name="seccion[]" readonly id="seccion" value="<?php echo $row['seccion']; ?>">
-    <td><input class="dias" type="text" name="estatus[]" readonly id="estatus" value="<?php echo $row['estatus_curso']; ?>"></td>
+    <td><input type="text" name="termina[]" readonly id="hora_termina" value="<?php echo $row['hora_termina']; ?>"></td>
+    <td><input type="text" name="dias[]" readonly id="dias" value="<?php echo $row['dias']; ?>"></td>
+    <td><input type="text" name="seccion[]" readonly id="seccion" value="<?php echo $row['seccion']; ?>">
+    <td><input type="text" name="periodo[]" readonly id="periodo" value="<?php echo $row['periodo'];?>"></td>
+    <td><input type="text" name="estatus[]" readonly id="estatus" value="<?php echo $row['estatus_curso']; ?>"></td>
     </tr>
     <?php
     }//end whil loop
     ?>
+
     </tbody>
 
-    </table>
+    </table><br>
     <input class="btn" type="submit" name="submit" value="Pre Matricular"/>
     </form>
 
@@ -82,6 +91,7 @@ require'header.php';
     $dias=$_POST['dias'];
     $seccion=$_POST['seccion'];
     $id=$_POST['id'];
+    $periodo=$_POST['periodo'];
     $estatus=$_POST['estatus'];
 
 
@@ -90,7 +100,7 @@ require'header.php';
        VALUES('".$row['asignatura']."',
             '".$row['seccion']."', '".$row['horario']."')");*/
 
-         $query=mysqli_query($conexion, "INSERT INTO prematriculas(num_cuenta, id_programacion, estatus_curso)
+         $query=mysqli_query($conexion, "INSERT INTO prematriculas(id_estudiante, id_programacion, estatus_curso)
        VALUES('$_SESSION[id_carrera]',
             '{$id [$i]}', '{$estatus [$i]}')");
 
