19282f05162af576b531a3d06c4136c7a2d57ee8
juanreyesmonroy <juanreyes@gmail.com>
2015-08-14 21:53:50 -0600

    Agregando campo olvidado en empleados

--- adminmatricula/editardocentes.php
+++ adminmatricula/editardocentes.php
@@ -5,7 +5,7 @@ header('Content-type: application/json');
 	//Update record in database
 
 
-		$result = mysqli_query($conexion,"UPDATE empleados SET codigo_empleado = '" . $_POST["codigo_empleado"] . "',nombres = '" . $_POST["nombres"] . "',genero = '" . $_POST["genero"] . "',telefono = '" . $_POST["telefono"] . "',email = '" . $_POST["email"] . "',codigo_carrera = '" . $_POST["codigo_carrera"] . "',id_puesto = '" . $_POST["id_puesto"] . "'  WHERE id_empleado = " . $_POST["id_empleado"] . ";");
+		$result = mysqli_query($conexion,"UPDATE empleados SET codigo_empleado = '" . $_POST["codigo_empleado"] . "',nombres = '" . $_POST["nombres"] . "',genero = '" . $_POST["genero"] . "',telefono = '" . $_POST["telefono"] . "',email = '" . $_POST["email"] . "',codigo_carrera = '" . $_POST["codigo_carrera"] . "',id_puesto = '" . $_POST["id_puesto"] . "',num_cuenta = '" . $_POST["num_cuenta"] . "'  WHERE id_empleado = " . $_POST["id_empleado"] . ";");
 
 
 			//Return result to jTable
