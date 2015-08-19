51fdc022a8e330763b3146c8155bad7c4491b07d
jorman-romero08 <jorman.romero@ujcv.edu.hn>
2015-08-18 21:32:47 -0600

    Interfaz Contabilidad

--- contabilidad/index.php
+++ contabilidad/index.php
@@ -17,7 +17,7 @@ while($row=mysqli_fetch_row($result))
       isset($_SESSION['apellido']) &&
       isset($_SESSION['usuario']) &&
       isset($_SESSION['tipo_usuario']) &&
-      isset($_SESSION['num_cuenta']) //hola
+      isset($_SESSION['num_cuenta'])
       )
    ){
       header('Location:../index.php?no_aut');
