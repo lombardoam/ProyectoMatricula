2facab149829b3d79d57741a656ed517c4ddbbf9
lombardoam <ardonmejial@gmail.com>
2015-08-19 21:45:59 -0600

    Arreglando empalmes

--- adminSistema/opciones/php/ingresar/ingresarcursos.php
+++ adminSistema/opciones/php/ingresar/ingresarcursos.php
@@ -2,22 +2,16 @@
 header('Content-type: application/json');
 require '../../require/conexion.php';
 
-$sql1 = mysqli_num_rows(mysqli_query($conexion, "SELECT programacion_cursos.hora_inicio, programacion_cursos.id_empleado, programacion_cursos.id_aula, cursos.periodo FROM programacion_cursos INNER JOIN cursos ON programacion_cursos.id_curso = cursos.id_curso WHERE programacion_cursos.hora_inicio = '".$_POST["hora_inicio"]."' AND programacion_cursos.id_aula = ".$_POST["id_aula"]." AND programacion_cursos.dias = '" . $_POST["dias"] . "' ")); /*Que no se pueda poner en un mismo dia, en una misma aula y en un mismo horario un curso*/
+$sql1 = mysqli_num_rows(mysqli_query($conexion, "SELECT programacion_cursos.hora_inicio, programacion_cursos.id_empleado, programacion_cursos.id_aula, cursos.periodo FROM programacion_cursos INNER JOIN cursos ON programacion_cursos.id_curso = cursos.id_curso WHERE programacion_cursos.hora_inicio = '".$_POST["hora_inicio"]."' AND programacion_cursos.id_aula = ".$_POST["id_aula"]." AND programacion_cursos.dias = '" . $_POST["dias"] . "' ")); /*Que no se pueda poner en un mismo dia, en una misma aula y en un mismo horario un curso*/ /*AQUI NO IMPORTA EL MAESTRO*/
 
-$sql2 = mysqli_num_rows(mysqli_query($conexion, "SELECT programacion_cursos.hora_inicio, programacion_cursos.id_empleado, programacion_cursos.id_aula, cursos.periodo FROM programacion_cursos INNER JOIN cursos ON programacion_cursos.id_curso = cursos.id_curso WHERE programacion_cursos.hora_inicio = '".$_POST["hora_inicio"]."' AND programacion_cursos.id_empleado = " . $_POST["id_empleado"] . " AND programacion_cursos.dias = '" . $_POST["dias"] . "' ")); /*No asignar un profesor a otra clase en una hora que el ya tiene ocupado y los mismos dias*/
+$sql2 = mysqli_num_rows(mysqli_query($conexion, "SELECT programacion_cursos.hora_inicio, programacion_cursos.id_empleado, programacion_cursos.id_aula, cursos.periodo FROM programacion_cursos INNER JOIN cursos ON programacion_cursos.id_curso = cursos.id_curso WHERE programacion_cursos.hora_inicio = '".$_POST["hora_inicio"]."' AND programacion_cursos.id_empleado = " . $_POST["id_empleado"] . " AND programacion_cursos.dias = '" . $_POST["dias"] . "' ")); /*No asignar un profesor a otra clase en una hora que el ya tiene ocupado y los mismos dias*/ /*AQUI NO IMPORTA EL AULA*/
 
-$sql3 = mysqli_query($conexion, "SELECT cursos.periodo FROM cursos INNER JOIN programacion.cursos ON cursos.id_curso = programacion_cursos.id_curso WHERE cursos.id_curso = " . $_POST["id_curso"] . " "); /*Solo sacando el periodo de cada curso que se programará*/
+/*$sql3 = mysqli_fetch_array(mysqli_query($conexion, "SELECT cursos.periodo FROM cursos INNER JOIN programacion.cursos ON cursos.id_curso = programacion_cursos.id_curso WHERE programacion_cursos.id_curso = ".$_POST["id_curso"]." ")); /*Solo sacando el periodo de cada curso que se programará*/
 
-while($periodo = mysqli_fetch_assoc($sql3)){
-
-}
-
-$sql4 = mysqli_num_rows(mysqli_query($conexion, "SELECT programacion_cursos.hora_inicio, programacion_cursos.id_empleado, programacion_cursos.id_aula, cursos.periodo FROM programacion_cursos INNER JOIN cursos ON programacion_cursos.id_curso = cursos.id_curso WHERE programacion_cursos.hora_inicio = '".$_POST["hora_inicio"]."' AND cursos.periodo = ".$periodo["periodo"]." ")); /*Que no choquen clases del mismo periodo*/
+/*$sql4 = mysqli_num_rows(mysqli_query($conexion, "SELECT programacion_cursos.hora_inicio, programacion_cursos.id_empleado, programacion_cursos.id_aula, cursos.periodo FROM programacion_cursos INNER JOIN cursos ON programacion_cursos.id_curso = cursos.id_curso WHERE programacion_cursos.hora_inicio = '".$_POST["hora_inicio"]."' AND cursos.periodo = ".$sql3["periodo"]." ")); /*Que no choquen clases del mismo periodo*/
 
 if($sql1 > 0 OR $sql2 > 0){
-    echo "Hay un error";
-}else if($sql4 > 0){
-    echo "Hay un erro";
+    echo "Error";
 }else{
 
 //Insert record into database
