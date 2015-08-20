5213658ace1eb2bdf22bb3123560a42cf8391cb7
juanreyesmonroy <juanreyes@gmail.com>
2015-08-20 02:45:55 -0600

    Queda pendiente solucionar en el query el filtro de historial para los alumnos con varios registros en el mismo

--- adminmatricula/matricula.php
+++ adminmatricula/matricula.php
@@ -223,12 +223,17 @@ $qcuentap = mysqli_query($conexion, $qcuentap);
  echo $lineaplan['nombre_plan'];
  $lineaplan['id_plan_estudio'];
  echo '</center></h3>';
-
-
-            $sql = "SELECT programacion_cursos.id_programacion, programacion_cursos.codigo_prog_curso, cursos.nombre_curso, planes_estudio.nombre_plan, programacion_cursos.dias, programacion_cursos.seccion, programacion_cursos.hora_inicio, programacion_cursos.hora_termina,  empleados.nombres, aulas.codigo_aula FROM programacion_cursos INNER JOIN cursos INNER JOIN planes_estudio INNER JOIN empleados INNER JOIN aulas INNER JOIN historiales_academicos WHERE programacion_cursos.id_curso = cursos.id_curso AND programacion_cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND programacion_cursos.id_empleado = empleados.id_empleado AND programacion_cursos.id_aula = aulas.id_aula AND programacion_cursos.id_plan_estudio='" . $lineaplan['id_plan_estudio'] . "' AND programacion_cursos.estatus_curso='Activo' AND (historiales_academicos.id_curso != programacion_cursos.id_curso AND historiales_academicos.num_cuenta='" . $_GET['numerocuenta'] . "' ) ORDER BY id_programacion;;";
+/*         $historiales = "SELECT programacion_cursos.id_curso, historiales_academicos.num_cuenta FROM programacion_cursos INNER JOIN historiales_academicos WHERE programacion_cursos.id_curso != historiales_academicos.id_curso AND historiales_academicos.num_cuenta='" . $_GET['numerocuenta'] . "'";
+     $historiales = mysqli_query($conexion, $historiales);
+     while($historiales = mysqli_fetch_assoc($historiales)){
+        $historiales['id_curso'];
+        $historiales['num_cuenta'];
+*/
+            $sql = "SELECT DISTINCT programacion_cursos.id_programacion, programacion_cursos.codigo_prog_curso, cursos.nombre_curso, planes_estudio.nombre_plan, programacion_cursos.dias, programacion_cursos.seccion, programacion_cursos.hora_inicio, programacion_cursos.hora_termina,  empleados.nombres, aulas.codigo_aula FROM programacion_cursos INNER JOIN cursos INNER JOIN planes_estudio INNER JOIN empleados INNER JOIN aulas INNER JOIN historiales_academicos WHERE programacion_cursos.id_curso = cursos.id_curso AND programacion_cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND programacion_cursos.id_empleado = empleados.id_empleado AND programacion_cursos.id_aula = aulas.id_aula AND programacion_cursos.id_plan_estudio='" . $lineaplan['id_plan_estudio'] . "' AND programacion_cursos.estatus_curso='Activo' NOT IN (historiales_academicos.id_curso = programacion_cursos.id_curso ) AND historiales_academicos.num_cuenta='" . $_GET['numerocuenta'] . "' ORDER BY id_programacion;;";
 
 $query = mysqli_query($conexion, $sql);
             while($rows = mysqli_fetch_assoc($query)){
+            $data[] = $rows;
                echo "    <tr>
     <td><input type='checkbox' class='checkthis' /></td>
     <td>$rows[codigo_prog_curso]</td>
@@ -243,8 +248,8 @@ $query = mysqli_query($conexion, $sql);
     ";
    }
  }
-
-}
+ }
+// }
 }
 
 
