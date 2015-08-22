18ae732eabbe100aca2a8c336f4d9f91b677c085
juanreyesmonroy <juanreyes@gmail.com>
2015-08-21 20:41:29 -0600

    Cambiando a método POST

--- adminmatricula/matricula.php
+++ adminmatricula/matricula.php
@@ -35,7 +35,7 @@ require 'noautorizado.php';
 
     <!-- /Inicio de código de formulario -->
 
-<form class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form name="form" class="form-horizontal" action="matricula.php" method="POST">
     <script src="js/table.js"></script>
 <fieldset>
 
@@ -57,8 +57,8 @@ require 'noautorizado.php';
     <div class="input-group">
       <span class="input-group-addon"><span class="fa fa-fw fa-user"></span>
        <?php
-            if (!empty ($_GET['numerocuenta'])){
-            $qcuenta = "SELECT nombres,apellidos FROM estudiantes WHERE num_cuenta ='" . $_GET['numerocuenta'] . "'";
+            if (!empty ($_POST['numerocuenta'])){
+            $qcuenta = "SELECT nombres,apellidos FROM estudiantes WHERE num_cuenta ='" . $_POST['numerocuenta'] . "'";
             $qcuenta = mysqli_query($conexion, $qcuenta);
             while($lineanombres = mysqli_fetch_assoc($qcuenta)){
             echo $lineanombres['nombres'];
@@ -67,7 +67,7 @@ require 'noautorizado.php';
             }
 
 
-$qcuenta = "SELECT nombres,apellidos FROM estudiantes WHERE num_cuenta ='" . $_GET['numerocuenta'] . "'";
+$qcuenta = "SELECT nombres,apellidos FROM estudiantes WHERE num_cuenta ='" . $_POST['numerocuenta'] . "'";
 $qcuenta = mysqli_query($conexion, $qcuenta);
 $registroscuenta = mysqli_num_rows($qcuenta);
     if($registroscuenta==0){
@@ -106,9 +106,9 @@ $registroscuenta = mysqli_num_rows($qcuenta);
     <div class="input-group">
       <span class="input-group-addon"><span class="fa fa-fw fa-money"></span>
         <?php
-if (!empty($_GET['numerocuenta'])){
+if (!empty($_POST['numerocuenta'])){
 
-$qcuenta = "SELECT saldo FROM estudiantes WHERE num_cuenta ='" . $_GET['numerocuenta'] . "'";
+$qcuenta = "SELECT saldo FROM estudiantes WHERE num_cuenta ='" . $_POST['numerocuenta'] . "'";
 $qcuenta = mysqli_query($conexion, $qcuenta);
  while($lineasaldo = mysqli_fetch_assoc($qcuenta)){
  echo 'L. ';
@@ -135,9 +135,9 @@ if (($lineasaldo['saldo'] != "0" OR $lineasaldo['saldo'] != "0.00" )) {
     <div class="input-group">
       <span class="input-group-addon"><span class="fa fa-fw fa-thumb-tack"></span>
         <?php
-if (!empty($_GET['numerocuenta'])){
+if (!empty($_POST['numerocuenta'])){
 
-$qtipo = "SELECT tipo_estudiante FROM estudiantes WHERE num_cuenta ='" . $_GET['numerocuenta'] . "'";
+$qtipo = "SELECT tipo_estudiante FROM estudiantes WHERE num_cuenta ='" . $_POST['numerocuenta'] . "'";
 $qtipo = mysqli_query($conexion, $qtipo);
  while($lineatipo = mysqli_fetch_assoc($qtipo)){
  echo $lineatipo['tipo_estudiante'];
@@ -162,7 +162,7 @@ $qtipo = mysqli_query($conexion, $qtipo);
 </div>
 
 </fieldset>
-</form>
+
 
   <!-- / Fin del código del formulario -->
 
@@ -181,6 +181,7 @@ $qtipo = mysqli_query($conexion, $qtipo);
                    <thead>
 
                    <th><input type="checkbox" id="checkall" /></th>
+                   <th>ID</th>
                    <th>Código</th>
                     <th>Nombre clase</th>
                     <th>Plan de Estudio</th>
@@ -205,9 +206,9 @@ generales de todas las carreras que están disponibles -->
 
         <!-- Primero buscando el plan de estudio del estudiante, al detectarlo procede a filtrar los horarios -->
 <?php
-if (!empty($_GET['numerocuenta'])){
+if (!empty($_POST['numerocuenta'])){
 
-$qcuentap = "SELECT planes_estudio.nombre_plan, planes_estudio.id_plan_estudio FROM `planes_estudio` INNER JOIN estudiantes INNER JOIN carreras WHERE estudiantes.id_carrera=carreras.id_carrera AND estudiantes.num_cuenta ='" . $_GET['numerocuenta'] . "' AND carreras.id_carrera=planes_estudio.id_carrera";
+$qcuentap = "SELECT planes_estudio.nombre_plan, planes_estudio.id_plan_estudio FROM `planes_estudio` INNER JOIN estudiantes INNER JOIN carreras WHERE estudiantes.id_carrera=carreras.id_carrera AND estudiantes.num_cuenta ='" . $_POST['numerocuenta'] . "' AND carreras.id_carrera=planes_estudio.id_carrera";
 $qcuentap = mysqli_query($conexion, $qcuentap);
     $registroscuenta = mysqli_num_rows($qcuenta);
     if($registroscuenta==0){
@@ -226,22 +227,25 @@ $qcuentap = mysqli_query($conexion, $qcuentap);
 /* Consulta que muestra las clases no aprobadas aún por el estudiante.
 
 */
-            $sql = "SELECT programacion_cursos.id_programacion, programacion_cursos.codigo_prog_curso, cursos.nombre_curso, planes_estudio.nombre_plan, programacion_cursos.dias, programacion_cursos.seccion, programacion_cursos.hora_inicio, programacion_cursos.hora_termina, empleados.nombres, aulas.codigo_aula FROM programacion_cursos INNER JOIN cursos INNER JOIN planes_estudio INNER JOIN empleados INNER JOIN aulas WHERE NOT EXISTS (SELECT * FROM historiales_academicos WHERE programacion_cursos.id_curso=historiales_academicos.id_curso AND historiales_academicos.num_cuenta='" . $_GET['numerocuenta'] . "' AND programacion_cursos.id_plan_estudio='" . $lineaplan['id_plan_estudio'] . "' AND historiales_academicos.estado='Aprobado') AND programacion_cursos.id_curso = cursos.id_curso AND programacion_cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND programacion_cursos.id_empleado = empleados.id_empleado AND programacion_cursos.id_aula = aulas.id_aula AND programacion_cursos.id_plan_estudio='" . $lineaplan['id_plan_estudio'] . "' AND programacion_cursos.estatus_curso='Activo'";
+            $sql = "SELECT programacion_cursos.id_programacion, programacion_cursos.codigo_prog_curso, cursos.nombre_curso, planes_estudio.nombre_plan, programacion_cursos.dias, programacion_cursos.seccion, programacion_cursos.hora_inicio, programacion_cursos.hora_termina, empleados.nombres, aulas.codigo_aula FROM programacion_cursos INNER JOIN cursos INNER JOIN planes_estudio INNER JOIN empleados INNER JOIN aulas WHERE NOT EXISTS (SELECT * FROM historiales_academicos WHERE programacion_cursos.id_curso=historiales_academicos.id_curso AND historiales_academicos.num_cuenta='" . $_POST['numerocuenta'] . "' AND programacion_cursos.id_plan_estudio='" . $lineaplan['id_plan_estudio'] . "' AND historiales_academicos.estado='Aprobado') AND programacion_cursos.id_curso = cursos.id_curso AND programacion_cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND programacion_cursos.id_empleado = empleados.id_empleado AND programacion_cursos.id_aula = aulas.id_aula AND programacion_cursos.id_plan_estudio='" . $lineaplan['id_plan_estudio'] . "' AND programacion_cursos.estatus_curso='Activo'";
+
+    $i = 0;
 
 $query = mysqli_query($conexion, $sql);
             while($rows = mysqli_fetch_assoc($query)){
             $data[] = $rows;
                echo "    <tr>
-    <td><input type='checkbox' class='checkthis' name='checar' /></td>
-    <td><input type='hidden' id='codigo' name='codigo' class='texto'>$rows[codigo_prog_curso]</input></td>
-    <td><input type='hidden' id='nombre' name='nombre' class='texto'>$rows[nombre_curso]</input></td>
-    <td><input type='hidden' id='plan' name='plan' class='texto'>$rows[nombre_plan]</input></td>
-    <td><input type='hidden' id='dias' name='dias' class='texto'>$rows[dias]</input></td>
-    <td><input type='hidden' id='seccion' name='seccion' class='texto'>$rows[seccion]</input></td>
-    <td><input type='hidden' id='inicio' name='inicio' class='texto'>$rows[hora_inicio]</input></td>
-    <td><input type='hidden' id='termina' name='termina' class='texto'>$rows[hora_termina]</input></td>
-    <td><input type='hidden' id='docente' name='docente' class='texto'>$rows[nombres]</input></td>
-    <td><input type='hidden' id='aula' name='aula' class='texto'>$rows[codigo_aula]</input></td>
+    <td><input type='checkbox' class='checkthis' name='eleccion[]' value='$1' /></td>
+    <td><input type='hidden' id='id' name='id[]' class='texto'>$rows[id_programacion]</input></td>
+    <td><input type='hidden' id='codigo' name='codigo[]' class='texto'>$rows[codigo_prog_curso]</input></td>
+    <td><input type='hidden' id='nombre' name='nombre[]' class='texto'>$rows[nombre_curso]</input></td>
+    <td><input type='hidden' id='plan' name='plan[]' class='texto'>$rows[nombre_plan]</input></td>
+    <td><input type='hidden' id='dias' name='dias[]' class='texto'>$rows[dias]</input></td>
+    <td><input type='hidden' id='seccion' name='seccion[]' class='texto'>$rows[seccion]</input></td>
+    <td><input type='hidden' id='inicio' name='inicio[]' class='texto'>$rows[hora_inicio]</input></td>
+    <td><input type='hidden' id='termina' name='termina[]' class='texto'>$rows[hora_termina]</input></td>
+    <td><input type='hidden' id='docente' name='docente[]' class='texto'>$rows[nombres]</input></td>
+    <td><input type='hidden' id='aula' name='aula[]' class='texto'>$rows[codigo_aula]</input></td>
     ";
    }
  }
@@ -252,13 +256,14 @@ $query = mysqli_query($conexion, $sql);
 
 //Si la búsqueda está vacía hace una muestra sin filtro de todos los horarios sin buscar el plan de estudio del estudiante
 
- if (empty($_GET['numerocuenta'])){
+ if (empty($_POST['numerocuenta'])){
             $sql = "SELECT programacion_cursos.id_programacion, programacion_cursos.codigo_prog_curso, cursos.nombre_curso, planes_estudio.nombre_plan, programacion_cursos.dias, programacion_cursos.seccion, programacion_cursos.hora_inicio, programacion_cursos.hora_termina,  empleados.nombres, aulas.codigo_aula FROM programacion_cursos INNER JOIN cursos INNER JOIN planes_estudio INNER JOIN empleados INNER JOIN aulas WHERE programacion_cursos.id_curso = cursos.id_curso AND programacion_cursos.id_plan_estudio = planes_estudio.id_plan_estudio AND programacion_cursos.id_empleado = empleados.id_empleado AND programacion_cursos.id_aula = aulas.id_aula AND programacion_cursos.estatus_curso='Activo' ORDER BY id_programacion;;";
 
 $query = mysqli_query($conexion, $sql);
             while($rows = mysqli_fetch_assoc($query)){
                echo "    <tr>
     <td><input type='checkbox' class='checkthis' /></td>
+    <td>$rows[id_programacion]</td>
     <td>$rows[codigo_prog_curso]</td>
     <td>$rows[nombre_curso]</td>
     <td>$rows[nombre_plan]</td>
@@ -301,7 +306,7 @@ $query = mysqli_query($conexion, $sql);
 <!-- Botón que inserta la información a la BD -->
     <?php
 
-if (empty($_GET['numerocuenta'])) {
+if (empty($_POST['numerocuenta'])) {
 echo '<div align="center">
 <button id="matricular" name="matricular" class="btn btn-primary disabled"><span class="fa fa-fw fa-check-square-o"></span> Matrícular</button>
 
@@ -315,8 +320,8 @@ $lineasaldo['saldo'];
 }
 
 }
-if (!empty($_GET['numerocuenta'])) {
-$qcuenta = "SELECT saldo FROM estudiantes WHERE num_cuenta ='" . $_GET['numerocuenta'] . "'";
+if (!empty($_POST['numerocuenta'])) {
+$qcuenta = "SELECT saldo FROM estudiantes WHERE num_cuenta ='" . $_POST['numerocuenta'] . "'";
 $qcuenta = mysqli_query($conexion, $qcuenta);
  while($lineasaldo = mysqli_fetch_assoc($qcuenta)){
 $lineasaldo['saldo'];
@@ -354,13 +359,39 @@ echo '<div align="center">
           </div></div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-        <button type="button" class="btn btn-primary">Sí</button>
+        <input type= "submit" class="btn btn-primary" name="submit" id="submit" value="Sí"/>
       </div>
     </div>
+
   </div>
 </div>
 
+<?php
+    if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $eleccion=$_POST['eleccion'];
+    $codigo=$_POST['codigo'];
+    $asignatura=$_POST['nombre'];
+    $plan=$_POST['plan'];
+    $dias=$_POST['dias'];
+    $seccion=$_POST['seccion'];
+    $inicio=$_POST['inicio'];
+    $termina=$_POST['termina'];
+    $docente=$_POST['docente'];
+    $aula=$_POST['aula'];
 
+
+    foreach($eleccion as $i){
+
+         $envio=mysqli_query($conexion, "INSERT INTO matriculas(num_cuenta, id_programacion)
+       VALUES('$_POST[numerocuenta]',
+            '{$id [$i]}')");
+   }
+    }
+
+   ?>
+
+ </form>
             <br><br>
 
 
