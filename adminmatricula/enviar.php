638f450689755e87128d531aad9bcea9a078ab89
juanreyesmonroy <juanreyes@gmail.com>
2015-08-25 21:55:12 -0600

    Ingresa pero no pilla datos del POST

---
+++ adminmatricula/enviar.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require 'conexion.php';
+
+require 'matricula.php';
+
+
+
+    if(isset($_POST['submit'])){
+    $checkthis=$_POST['check'];
+    $id=$_POST['id_programacion'];
+;
+
+
+    foreach($checkthis as $i){
+
+        $query = mysqli_query($conexion, "INSERT INTO matriculas (num_cuenta, id_programacion)
+       VALUES('{$id_estu}', '{$id [$i]}')");
+
+   }
+    }
+
+?>
