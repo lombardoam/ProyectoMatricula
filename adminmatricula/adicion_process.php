6d6d40d5e9530013eeb40600196a12531bde4647
waldos13 <wajosos1960@gmail.com>
2015-08-26 04:49:06 -0600

    adiciones funcionando al 100%

--- adminmatricula/adicion_process.php
+++ adminmatricula/adicion_process.php
@@ -10,11 +10,13 @@ require 'adiciones.php';
     if(isset($_POST['submit'])){
     $checkthis=$_POST['check'];
     $id=$_POST['id_programacion'];
+    $cuenta=$_POST['cuenta'];
+;
 
-    foreach($checkthis as $i){
+    for($i = 0; $i<count($checkthis); $i++){
 
         $query = mysqli_query($conexion, "INSERT INTO matriculas (num_cuenta, id_programacion)
-       VALUES('{$id_estu}', '{$id [$i]}')");
+       VALUES('{$cuenta [$i]}', '{$id [$i]}')");
 
    }
     }
